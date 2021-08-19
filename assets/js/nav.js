71a6227d86a47707e4cc4639816c171ac4e2913b
Kenny Stephens <kennethcstephens@gmail.com>
2021-08-19 16:20:29 -0700

    more updates to nav

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,10 +1,12 @@
 $('.navTrigger').click(function () {
     $(this).toggleClass('active');
-    console.log("Clicked menu");
     $("#mainListDiv").toggleClass("show_list");
     $("#mainListDiv").fadeIn();
-
 });
+
+function closeMobileMenu() {
+    $("#mainListDiv").removeClass("show_list");
+}
 
 $(window).scroll(function() {
     if ($(document).scrollTop() > 50) {
